4bd29605e8109f5b391420779e43d9743a605000
Sergio Torres <sergiotirres@gmail.com>
2019-10-16 12:38:52 -0700

    1st commit

--- ConsoleApp1/MyShape.cs
+++ ConsoleApp1/MyShape.cs
@@ -6,17 +6,155 @@ namespace ConsoleApp1
 {
     class MyShape
     {
+        public List<Vector2> MyPoints = new List<Vector2>();
         public Vector2 position = new Vector2();
-        public List<Raylib.Vector2> MyPoints = new List<Vector2>();
+        public List<Vector2> myGlobal = new List<Vector2>();
+        public Color myColor;
+        public AABB myBox = new AABB();
         public void Draw()
         {
-            Vector2 Last = new Vector2(); 
-            for(int idx = 0; idx < MyPoints.Count; idx++)
+            Vector2 Last = new Vector2();
+            for (int idx = 0; idx < MyPoints.Count; idx++)
             {
-                if(idx>0)
-                Raylib.Raylib.DrawLineEx(position+Last, position+MyPoints[idx], 2, Color.BLACK);
+                if (idx > 0)
+                {
+                    Vector2 tmp = position + Last;
+                    Vector2 tmp2 = position + MyPoints[idx];
+                    Raylib.Raylib.DrawLineEx(new Raylib.Vector2(tmp.x, tmp.y), new Raylib.Vector2(tmp2.x, tmp2.y), 2, myColor);
+                }
                 Last = MyPoints[idx];
             }
         }
+    }
+
+    public class AABB
+    {
+        Vector2 min = new Vector2(float.NegativeInfinity,
+                                  float.NegativeInfinity);
+
+        Vector2 max = new Vector2(float.PositiveInfinity,
+                                  float.PositiveInfinity);
+
+        public AABB()
+        {
+
+        }
+
+        public AABB(Vector2 min, Vector2 max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public Vector2 Center()
+        {
+            return (min + max) * 0.5f;
+        }
+
+        public Vector2 Extents()
+        {
+            return new Vector2(Math.Abs(max.x - min.x) * 0.5f,
+                               Math.Abs(max.y - min.y) * 0.5f);
+        }
+
+        public List<Vector2> Corners()
+        {
+            //ignoring z axis for 2D
+            List<Vector2> corners = new List<Vector2>();
+            corners[0] = min;
+            corners[1] = new Vector2(min.x, max.y);
+            corners[2] = max;
+            corners[3] = new Vector2(max.x, min.y);
+            return corners;
+        }
+
+        public void Fit(List<Vector2> points)
+        {
+            //invalidate the extends
+            min = new Vector2(float.PositiveInfinity,
+                              float.PositiveInfinity);
+
+            max = new Vector2(float.NegativeInfinity,
+                              float.NegativeInfinity);
+
+            //find min and max of the points
+            foreach (Vector2 p in points)
+            {
+                min = Vector2.Min(min, p);
+                max = Vector2.Max(max, p);
+            }
+
+        }
+
+        public bool Overlaps(Vector2 p)
+        {
+            //test for not overlapped as it exits faster
+            return !(p.x < min.x || p.y < min.y || p.x > max.x || p.y > max.y);
+        }
+
+        public bool Overlaps(AABB other)
+        {
+            return !(max.x < other.min.x || max.y < other.min.y || min.x > other.max.x || min.y > other.max.y);
+        }
+
+        public Vector2 ClosestPoint(Vector2 p)
+        {
+            return Vector2.Clamp(p, min, max);
+        }
+    }
+
+    public struct Vector2
+    {
+        public float x, y;
+
+        public Vector2(float a, float b)
+        {
+            x = a;
+            y = b;
+
+        }
+        public static Vector2 operator *(Vector2 lhs, float rhs)
+        {
+            return new Vector2(
+            lhs.x * rhs,
+            lhs.y * rhs);
+        }
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            return new Vector2(
+            a.x - b.x,
+            a.y - b.y);
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x + b.x, a.y + b.y);
+        }
+
+        public float Dot(Vector2 rhs, float x, float y)
+        {
+            return x * rhs.x + y * rhs.y;
+        }
+
+        public float MagnitudeSqr()
+        {
+            return x * x + y * y;
+        }
+
+        public static Vector2 Min(Vector2 a, Vector2 b)
+        {
+            return new Vector2(Math.Min(a.x, b.x), Math.Min(a.y, b.y));
+        }
+
+        public static Vector2 Max(Vector2 a, Vector2 b)
+        {
+            return new Vector2(Math.Max(a.x, b.x), Math.Max(a.y, b.y));
+        }
+
+        public static Vector2 Clamp(Vector2 t, Vector2 a, Vector2 b)
+        {
+            return Max(a, Min(b, t));
+        }
+
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,15 +17,27 @@ namespace ConsoleApp1
             rl.SetTargetFPS(60);
             //--------------------------------------------------------------------------------------
             MyShape triangle = new MyShape();
-            triangle.MyPoints.Add(new Vector2(10, 10));
-            triangle.MyPoints.Add(new Vector2(20, 30));
-            triangle.MyPoints.Add(new Vector2(30, 10));
-            triangle.MyPoints.Add(new Vector2(10, 10));
-            triangle.MyPoints.Add(new Vector2(40, 30));
-            triangle.MyPoints.Add(new Vector2(50, 10));
+            triangle.MyPoints.Add(new Vector2(-20, 20));
+            triangle.MyPoints.Add(new Vector2(0, -20));
+            triangle.MyPoints.Add(new Vector2(20, 20));
+            triangle.MyPoints.Add(new Vector2(-20, 20));
             triangle.position = new Vector2(100, 100);
 
+
+
             //TODO:Create another object with a different shape
+
+            MyShape rectangle = new MyShape();
+            rectangle.MyPoints.Add(new Vector2(-20, 20));
+            rectangle.MyPoints.Add(new Vector2(-20, -20));
+            rectangle.MyPoints.Add(new Vector2(20, -20));
+            rectangle.MyPoints.Add(new Vector2(20, 20));
+            rectangle.MyPoints.Add(new Vector2(-20, 20));
+            rectangle.position = new Vector2(300, 100);
+
+
+            float tv = 0.5f;
+            float sv = -0.5f;
 
 
             // Main game loop
@@ -34,6 +46,8 @@ namespace ConsoleApp1
                 // Update
                 //----------------------------------------------------------------------------------
                 // TODO: Update your variables here
+
+
                 //----------------------------------------------------------------------------------
 
                 // Draw
@@ -43,19 +57,90 @@ namespace ConsoleApp1
                 rl.ClearBackground(Color.RAYWHITE);
 
                 rl.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LIGHTGRAY);
+
+                if (triangle.position.x > 800)
+                {
+                    triangle.position.x = 5;
+                }
+
+                if (triangle.position.x < 0)
+                {
+                    triangle.position.x = 795;
+                }
+
+                if (triangle.position.y > 450)
+                {
+                    triangle.position.y = 5;
+                }
+
+                if (triangle.position.y < 0)
+                {
+                    triangle.position.y = 445;
+                }
+
+                if (rectangle.position.x > 800)
+                {
+                    rectangle.position.x = 5;
+                }
+
+                if (rectangle.position.x < 0)
+                {
+                    rectangle.position.x = 795;
+                }
+
+                if (rectangle.position.y > 450)
+                {
+                    rectangle.position.y = 5;
+                }
+
+                if (rectangle.position.y < 0)
+                {
+                    rectangle.position.y = 445;
+                }
+
+
                 triangle.Draw();
-                triangle.position.x += .2f;
+                triangle.myColor = Color.DARKGREEN;
+
+                rectangle.Draw();
+                rectangle.myColor = Color.DARKGREEN;
+
+                triangle.position.x += tv;
+                rectangle.position.x += sv;
+
+                foreach (Vector2 v in triangle.MyPoints)
+                {
+                    triangle.myGlobal.Add(v + triangle.position);
+                }
+                foreach (Vector2 v in rectangle.MyPoints)
+                {
+                    rectangle.myGlobal.Add(v + rectangle.position);
+                }
+
+                triangle.myBox.Fit(triangle.myGlobal);
+                rectangle.myBox.Fit(rectangle.myGlobal);
+
+                if (triangle.myBox.Overlaps(rectangle.myBox))
+                {
+                    tv = -0.5f;
+                    sv = 0.5f;
+
+                    triangle.myColor = Color.RED;
+                    rectangle.myColor = Color.RED;
+                }
+
+
 
                 //TODO:Move the 2nd object so that it is on a collision course with the triangle
                 //TODO:Implement AABB Collision detection so you know when they hit.
                 //Recommend adding the AABB Functionality to the myshape class.
                 //Add a method to the myshape class that causes the Bounding box to be recalculated and 
                 //stored in the myshape class (with the corners/vectors relative to itself)
-                
+
 
                 //TODO:Bonus have your AABB Box drawn as a green outline to the shapes, and then turn red 
                 //When they collide.
-                
+
 
                 rl.EndDrawing();
                 //----------------------------------------------------------------------------------
